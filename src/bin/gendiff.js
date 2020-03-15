f76b6cd338d3e7db9c5c405e8b552fb686025686
Оксана Дуга <oksanaduga@Oksanas-MacBook-Air.local>
2020-03-15 15:35:24 +0300

    add gendiff bin

---
+++ src/bin/gendiff.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+
+import program from 'commander';
+
+program
+.description('Compares two configuration files and shows a difference.')
+.version('0.1.0');
+
+
+program.parse(process.argv);
