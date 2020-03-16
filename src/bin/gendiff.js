868cf883b88b1aa101e3ff2fccaf84d8cbe35497
Оксана Дуга <oksanaduga@Oksanas-MacBook-Air.local>
2020-03-16 17:39:57 +0300

    change gendiff

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -4,7 +4,12 @@ import program from 'commander';
 
 program
 .description('Compares two configuration files and shows a difference.')
-.version('0.1.0');
+.version('0.1.0')
+.option('-f, --format [type]', 'output format', (a, b) => console.log(a, b))
+.arguments('<firstConfig> <secondConfig>')
+.action(function (cmd, env) {
+  console.log('dd');
+});
 
 
 program.parse(process.argv);
