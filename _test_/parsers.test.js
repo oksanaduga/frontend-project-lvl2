e8b89b44f96ae145aafb955b950302b5bee1af61
oksanaduga <akakiikaramazov@gmail.com>
2020-03-24 17:29:11 +0300

    add ini format

--- _test_/parsers.test.js
+++ _test_/parsers.test.js
@@ -1,29 +1,35 @@
 import parse from '../src/parsers.js';
+import fs from 'fs';
 
 const path = __dirname;
+const beforeObject = JSON.parse(fs.readFileSync(`${path}/fixtures/beforeObject.json`, 'utf8'));
+const beforeObjectAfterFormatIni = JSON.parse(fs.readFileSync(`${path}/fixtures/beforeObjectAfterFormatIni.json`, 'utf8'));
 
 test.each([
-  [`${path}/fixtures/before.json`, `${path}/fixtures/after.json`,
+  [`${path}/fixtures/before.json`,
     {
       host: "hexlet.io",
       timeout: 50,
       proxy: "123.234.53.22",
       follow: false
     }],
-  [`${path}/fixtures/before2.yml`, `${path}/fixtures/after2.yml`,
+  [`${path}/fixtures/before.yml`,
     {
       host: "hexlet.io",
       timeout: 50,
       proxy: 123,
       follow: false
     }],
-  [`${path}/fixtures/before3.ini`, `${path}/fixtures/after3.ini`,
+  [`${path}/fixtures/before.ini`,
     {
       host: "hexlet.io",
       timeout: "50",
       proxy: "123",
       follow: false
     }],
-])('.add(%s, %o)', (a, b, expected) => {
-  expect(parse(a, b)).toStrictEqual(expected);
+    [`${path}/fixtures/beforeNested.json`, beforeObject],
+    [`${path}/fixtures/beforeNested.yml`, beforeObject],
+    [`${path}/fixtures/beforeNested.ini`, beforeObjectAfterFormatIni],
+])('.add(%s, %o)', (a, expected) => {
+  expect(parse(a)).toStrictEqual(expected);
 });
--- src/index.js
+++ src/index.js
@@ -3,29 +3,27 @@
 import { has } from 'lodash';
 import parse from './parsers.js';
 
-
-
 const diff = (beforePath, afterPath) => {
   const objBefore = parse(beforePath);
   const objAfter = parse(afterPath);
 
-  let acc = "";
+  let acc = {};
   const newObj = { ...objBefore, ...objAfter };
   const arrOfKeys = Object.keys(newObj);
   for (let key of arrOfKeys) {
     if(has(objBefore, [key]) && has(objAfter, [key])) {
       if (objBefore[key] === objAfter[key]) {
-        acc += `\n   ${[key]}: ${objAfter[key]}`;
+        acc = { ...acc, { sign: "   ", key: key, value: objAfter[key] } };
       } else {
-        acc += `\n + ${[key]}: ${objAfter[key]}\n - ${[key]}: ${objBefore[key]}`;
+        acc = { ...acc, { "sign": " + ", "key": key, "value": objAfter[key]}, { "sign": " - ", "key": key, "value": objBefore[key]} };
       }
   } else if (has(objBefore, [key])) {
-      acc += `\n - ${[key]}: ${objBefore[key]}`;
+      acc = { ...acc, { "sign": " - ", "key": key, "value": objBefore[key]} };
     } else {
-      acc += `\n + ${[key]}: ${objAfter[key]}`;
+      acc = { ...acc, { "sign": " + ", "key": key, "value": objAfter[key]} };
     }
   }
-  return `{${acc}\n}`;
+  return acc;
   }
 
 export default diff;
